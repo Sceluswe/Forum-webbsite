359b7b8d02acee8b31f25570db5e7b4c84de49e4
Emil Mattsson <emilcbm@gmail.com>
2019-03-24 01:32:17 +0100

    installed dependencies, created db, new config_sqlite

---
+++ Kmom07/app/config/config_sqlite.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Set up details on how to connect to the database
+    'dsn'     => "sqlite:db/smite.sqlite",
+
+    // Display details on what happens: debug mode
+    'verbose' => false,
+
+    // Throw a more verbose exception when failing to connect
+    //'debug_connect' => 'true',
+];
