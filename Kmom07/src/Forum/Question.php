2208ede29bf5ff01a9d2579dd8d350570dedb148
Emil Mattsson <emilcbm@gmail.com>
2019-08-31 16:02:00 +0200

    Question.php: add getByTag() to get tagged questions efficiently

--- Kmom07/src/Forum/Question.php
+++ Kmom07/src/Forum/Question.php
@@ -29,4 +29,15 @@ class Question extends \Anax\MVC\CDatabaseModel
 	{
 		return $this->session->get('savedQuestion');
 	}
+
+    public function selectByTag($tagId)
+    {
+        $this->db->select()
+            ->from($this->getSource() . ", questions_tags")
+            ->where("questions_tags.tagId = ?")
+            ->andWhere("questions_tags.questionId = Question.id");
+
+        $this->db->execute([$tagId]);
+        return $this->db->fetchAll();
+    }
 }
