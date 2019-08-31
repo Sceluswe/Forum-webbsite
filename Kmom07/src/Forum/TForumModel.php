92db26c2fc7d2ff020d3c6f599827f776c353d09
Emil Mattsson <emilcbm@gmail.com>
2019-08-31 04:08:55 +0200

    TForumModel.php: implement set and getRedirect() for use in forum models

--- Kmom07/src/Forum/TForumModel.php
+++ Kmom07/src/Forum/TForumModel.php
@@ -3,6 +3,30 @@ namespace Anax\Forum;
 
 trait ForumModel {
 
+    /*
+	* Set route to redirect to in session.
+	*
+	* @param string, route to redirect to.
+	*
+	* @return string.
+	*/
+	public function setRedirect($key)
+	{
+		$this->session->set('redirect' + parent::getSource(), $key);
+	}
+
+
+
+	/*
+	* Get route to redirect to from session.
+	*
+	* @return string.
+	*/
+	public function getRedirect()
+	{
+		return $this->session->get('redirect' + parent::getSource());
+	}
+
 
 
     /**
