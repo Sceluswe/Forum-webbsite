2a7b804999fc88d6734a5ad5cbcdd07f16048b13
Emil Mattsson <emilcbm@gmail.com>
2019-09-04 13:25:38 +0200

    forum-tagMenu.ptl.pgp: use tag name in URL instead of id

--- Kmom07/app/view/forum/forum-tagMenu.tpl.php
+++ Kmom07/app/view/forum/forum-tagMenu.tpl.php
@@ -1,5 +1,5 @@
 <h2 style="margin-top: 24px;"><?=$title?></h2>
 <?php foreach($tags as $tag):?>
-	<a href="<?=$this->url->create("{$redirect['menu']}{$tag->id}")?>" class="tag"><?=$tag->name?></a>
+	<a href="<?=$this->url->create("{$redirect['menu']}{$tag->name}")?>" class="tag"><?=$tag->name?></a>
 <?php endforeach;?>
 <hr>
