ef4f1e4b7b236aaa88d84e57db3b88f3059d34f4
Emil Mattsson <emilcbm@gmail.com>
2019-10-09 05:20:25 +0200

    implement model linkQuestionToUserVotes in ForumController

--- Kmom07/src/Forum/linkQuestionToUserVotes.php
+++ Kmom07/src/Forum/linkQuestionToUserVotes.php
@@ -6,8 +6,25 @@ namespace Anax\Forum;
 *
 * Contains interactions with the database.
 */
-class QuestionTags extends \Anax\MVC\CDatabaseModel
+class linkQuestionToUserVotes extends \Anax\MVC\CDatabaseModel
 {
+
+
+
+    /**
+    * Checks if a user has voted on a question.
+    *
+    * @param string, the id of the question.
+    * @param string, the id of the user to look for.
+    *
+    * @return boolean, true or false.
+    */
+    public function userHasNotVoted($questionId, $userId)
+    {
+        return empty($this->query()->where('questionId = ?')
+            ->andWhere('userId = ?')
+            ->execute([$questionId, $userId]));
+    }
 
 
 
@@ -24,5 +41,42 @@ class QuestionTags extends \Anax\MVC\CDatabaseModel
         return !empty($this->query()->where('questionId = ?')
             ->andWhere('userId = ?')
             ->execute([$questionId, $userId]));
+    }
+
+
+
+    /**
+    * Checks if a user has voted on a question.
+    *
+    * @param string, the id of the question.
+    * @param string, the id of the user to look for.
+    *
+    * @return boolean, true or false.
+    */
+    public function addUserVote($questionId, $userId)
+    {
+        return $this->create([
+            "questionId"    => $questionId,
+            "userId"        => $userId
+        ]);
+    }
+
+
+
+    /**
+    * Checks if a user has voted on a question.
+    *
+    * @param string, the id of the question.
+    * @param string, the id of the user to look for.
+    *
+    * @return boolean, true or false.
+    */
+    public function removeUserVote($questionId, $userId)
+    {
+        $row = $this->query()->where('questionId = ?')
+            ->andWhere('userId = ?')
+            ->execute([$questionId, $userId])[0];
+
+        return $this->delete($row->id);
     }
 }
