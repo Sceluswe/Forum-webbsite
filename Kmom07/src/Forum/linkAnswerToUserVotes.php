4bd1d22958a57e125e0dbed16011fea5ededdbbf
Emil Mattsson <emilcbm@gmail.com>
2019-10-09 05:45:27 +0200

    create linkAnswerToUserVotes model for link table

---
+++ Kmom07/src/Forum/linkAnswerToUserVotes.php
@@ -0,0 +1,12 @@
+<?php
+namespace Anax\Forum;
+
+/**
+* Model for linking answer and user votes link table.
+*
+*/
+class linkAnswerToUserVotes extends \Anax\MVC\CDatabaseModel
+{
+    use \Anax\Forum\TlinkUserVotes;
+
+}
